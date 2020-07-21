5894e08f7bbf83e0d8bec433c2f5faf8cc3aa8be
Jefferson Arellano <Jefferson.Arellano@travelport.com>
2020-07-21 17:25:15 +0200

    http testing

--- src/utils/196.async.jsx
+++ src/utils/196.async.jsx
@@ -7,3 +7,34 @@ export const getDataCallback = (callback) => {
     callback(name);
   }, 2000);
 };
+
+export const getDataPromise = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Lupita");
+    }, 300);
+  });
+};
+
+export const getDataPromiseError = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("Error");
+    }, 300);
+  });
+};
+
+export const getUsers = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+  return users;
+};
+
+//{then, catch}
+// getDataPromise()
+//   .then((name) => {
+//     console.log(name);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
--- src/utils/__test__/196.async.test.jsx
+++ src/utils/__test__/196.async.test.jsx
@@ -1,4 +1,9 @@
-import { getDataCallback } from "../196.async";
+import {
+  getDataCallback,
+  getDataPromise,
+  getDataPromiseError,
+  getUsers,
+} from "../196.async";
 
 describe("Testing asynchronics calls", () => {
   test("Callback test", (done) => {
@@ -6,5 +11,53 @@ describe("Testing asynchronics calls", () => {
       expect(name).toBe("Lupe");
       done();
     });
+  });
+
+  test("Promise test", (done) => {
+    getDataPromise()
+      .then((name) => {
+        expect(name).toBe("Lupita");
+        done();
+      })
+      .catch(() => {});
+  });
+
+  test("Promise test with expect", () => {
+    return expect(getDataPromise()).resolves.toBe("Lupita");
+  });
+
+  test("Promise test reject", (done) => {
+    getDataPromiseError()
+      .then((name) => {})
+      .catch((error) => {
+        expect(error).toBe("Error");
+        done();
+      });
+  });
+
+  test("Promise test reject with expect", () => {
+    return expect(getDataPromiseError()).rejects.toBe("Error");
+  });
+
+  test("Promise test resolve async Await", async () => {
+    const name = await getDataPromise();
+    expect(name).toBe("Lupita");
+  });
+
+  test("Promise test reject async Await", async () => {
+    try {
+      const name = await getDataPromiseError();
+      expect(name).toBe("Lupita");
+    } catch (error) {
+      expect(error).toBe("Error");
+    }
+  });
+
+  test("Testing promise with HTTP ", async () => {
+    const users = await getUsers();
+
+    expect(users).toHaveProperty("username");
+    expect(users).toHaveProperty("id");
+    expect(users).toHaveProperty("email");
   });
 });
