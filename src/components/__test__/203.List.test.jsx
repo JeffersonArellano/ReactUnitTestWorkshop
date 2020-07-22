ff023a4d603d0ebf1a49a25517958e89427ca485
Jefferson Arellano <Jefferson.Arellano@travelport.com>
2020-07-22 09:37:46 +0200

    snapshot testing

--- src/components/__test__/203.List.test.jsx
+++ src/components/__test__/203.List.test.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { configure, shallow, mount, render } from "enzyme";
+import toJson from "enzyme-to-json";
 import Adapter from "enzyme-adapter-react-16";
 import List from "../203.List";
 
@@ -8,17 +9,21 @@ configure({
 });
 
 describe("Testing  list component ", () => {
+  const title = "Fruits List";
+  const fruits = [
+    { id: 1, name: "Orange" },
+    { id: 2, name: "Apple" },
+    { id: 3, name: "Strawberry" },
+    { id: 4, name: "Mango" },
+  ];
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<List title={title} list={fruits} />);
+  });
+
   test("nodes validation", () => {
-    const title = "Fruits List";
-    const fruits = [
-      { id: 1, name: "Orange" },
-      { id: 2, name: "Apple" },
-      { id: 3, name: "Strawberry" },
-      { id: 4, name: "Mango" },
-    ];
-
-    const wrapper = shallow(<List title={title} list={fruits} />);
-
     //Exisst  h1 node
     expect(wrapper.find("h1").exists()).toBe(true);
 
@@ -35,7 +40,29 @@ describe("Testing  list component ", () => {
     //Validate kind of node/element
     expect(wrapper.find("ul").childAt(2).type()).toBe("li");
 
-    //Validate content
+    //Validate html content
     expect(wrapper.find("ul").childAt(2).html()).toBe("<li>Strawberry</li>");
+  });
+
+  test("Validation on props update", () => {
+    //validate items quantity
+    expect(wrapper.find("li").length).toBe(4);
+
+    wrapper.setProps({
+      list: [{ id: 5, name: "Kiwi" }],
+    });
+
+    //validate items quantity
+    expect(wrapper.find("li").length).toBe(1);
+
+    wrapper.setProps({
+      title: "Title changed",
+    });
+
+    expect(wrapper.find(".big").text()).toBe("Title changed");
+  });
+
+  test("validate snapshot match", () => {
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
